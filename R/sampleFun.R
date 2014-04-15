7d8b3881763c0f5631fa38cd660fd446f3077d42
Luca Pozzi <luca@skytree.net>
2014-04-15 07:55:58 -0700

    fixed bug in the way sdev is handled

--- R/sampleFun.R
+++ R/sampleFun.R
@@ -123,7 +123,7 @@ function(n0,muTab, bounds,controlCol=NULL,controlRow=NULL,nIter=100,N=10000,sdev
 						###VARSTOCK not structural 0
 						
 						#if(length(rrow[-nc])!=nc-1)browser()
-						rrow[-nc] <- round(rtnorm(nc-1, mean=unlist(muTab[i,-nc]), sd=sdev[,-nc], lower=bounds[i,-nc,1],upper=bounds[i,-nc,2]),0)
+						rrow[-nc] <- round(rtnorm(nc-1, mean=unlist(muTab[i,-nc]), sd=sdev[-nc], lower=bounds[i,-nc,1],upper=bounds[i,-nc,2]),0)
 						rrow[nc] <- -(n0[i]-sum(rrow[-nc]))
 						
 						}
