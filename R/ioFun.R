a2da1750448bf9ece8016c7d73825423fa87dddf
marcogarieri <marcogarieri@gmail.com>
2014-10-01 17:33:12 +0200

    Group or Aggregate

--- R/ioFun.R
+++ R/ioFun.R
@@ -57,4 +57,67 @@ readFBS <- function(file,file0=NULL,filef,whichCols=c("Imports.primary","Exports
 	countryData
 	
 	}
+	
+	
+	
+	
+readFBS_group <- function(file,file0=NULL,filef,whichCols=c("Imports.primary","Exports.primary","Feed.use","Seed.use","Losses","Industrial.use","Food.use","Stock.change"),fixed="Production",sdCols=c("Imports.sd","Exports.sd"),whichRowsNot=c("GRAND TOTAL")){
+	
+	Sys.setlocale(locale="C")
+	rawData <- scan(file, what="", sep="\n",quote="\"")
+	header <- rawData[1]; rawData <- rawData[-1]
+	header  <- unlist(strsplit(header, ","))
+	rawData <- strsplit(rawData, ",")
+	rawData <- rawData[sapply(rawData,length)!=1]
+	
+	colN <- unlist(lapply(strsplit(whichCols,".",fixed=TRUE),"[[",1))
+	
+	structZero <- NULL
+	if(!is.null(file0)){
+		structZero <- read.csv(file0, row.names=2)[,-2]!="num"
+	}
+	feedConstraints <- NULL
+	if(!is.null(filef)){
+		feedConstraints <- read.csv(filef)
+		# Removing duplicated rows
+		feedConstraints  <- feedConstraints[!duplicated(feedConstraints),]
+	}
+	countries <- sapply(rawData, `[`,2)
+	names(countries) <- sapply(rawData, `[`,1)
+	countryData <- tapply(rawData, countries,function(country){
+		years <- sapply(country, `[`,4)
+		yearData <- tapply(country,years,function(year){
+			nm <- sapply(year,`[`,3)
+			fbs <- t(sapply(year,function(y) as.numeric(y[-(1:4)])))
+			rownames(fbs) <- nm
+			colnames(fbs) <- header[-(1:4)]
+			fbs[, whichCols[1:2]][fbs[,sdCols]==0] <- NA
+			fbs[, whichCols[1]] <- -fbs[, whichCols[1]]	
+			# if(!is.null(structZero)){
+				# fbs[, whichCols[-(1:2)]][fbs[, whichCols[-(1:2)]]==0 & structZero[rownames(fbs),]] <- NA
+				# }
+			codeYear <- t(sapply(year,`[`,c(2,4)))
+			codeYear  <- codeYear[!duplicated(codeYear,margin=2),]
+			## Should we remove GRAND TOTAL?
+			## Since we made the control on the total of the columns, then we need to remove GRAND TOTAL
+			#browser()
+			feed <- feedConstraints[feedConstraints[,1]==(codeYear[1])&feedConstraints[,2]==codeYear[2],3]
+			if(length(feed)>1) {
+				if(length(unique(feed))>1){
+					warning(paste("Constraints on Feed for",year[[1]][1],year[[1]][5],"have multiple values (choosing first).",sep=" "))
+					}
+				feedConstraints <- unique(feedConstraints)[1]
+			}
+			data <- fbs[!rownames(fbs) %in% whichRowsNot, whichCols]
+			colnames(data) <- colN
+			list(data=data,row_Tot=fbs[!rownames(fbs) %in% whichRowsNot,fixed],sd=fbs[!rownames(fbs) %in% whichRowsNot,sdCols],feed=feed)
+			})
+		
+		yearData
+		
+		})
 
+	attr(countryData,"countryMap") <- countries[!duplicated(countries)] #Fun Fact: unique removes names.
+	countryData
+	
+	}
