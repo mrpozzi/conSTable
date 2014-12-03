b4dcd311abe47e3bb4448630cad7e83abe3ffff4
Luca Pozzi <luca@skytree.net>
2014-12-03 09:35:33 -0800

    baco di nata

--- R/ioFun.R
+++ R/ioFun.R
@@ -18,7 +18,7 @@ readFBS <- function(file,file0=NULL,filef,whichCols=c("Imports.total","Exports.t
 	if(!is.null(filef)){
 		feedConstraints <- read.csv(filef)
 		if(ncol(feedConstraints)>3) {
-			feedConstraints <- feedConstraints[,c(1,3,5,6,7)]
+			feedConstraints <- feedConstraints[,c(1,3,6,7)]#5,
 		} 
 		# Removing duplicated rows
 		feedConstraints  <- feedConstraints[!duplicated(feedConstraints),]
